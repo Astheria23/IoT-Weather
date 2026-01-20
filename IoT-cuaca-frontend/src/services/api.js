fcc149c2d4a638d556e26ce056ec25c73261cede
astheria <octa.toriq@gmail.com>
2026-01-20 09:30:26 +0700

    update API base URL to use production endpoint

--- IoT-cuaca-frontend/src/services/api.js
+++ IoT-cuaca-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://dw3736vv-3000.asse.devtunnels.ms/";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
